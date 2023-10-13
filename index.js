34161229d74b95429a1381f169c0b0c1427fff48
victormuteithia <muteithiavictor@gmail.com>
2023-10-13 18:21:23 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(blockNumber){
+    const hqBlock = 42
+    if(blockNumber < 42){
+        return hqBlock-blockNumber
+    }
+    return blockNumber-hqBlock
+    }
+
+function distanceFromHqInFeet(blockNumber){
+        return distanceFromHqInBlocks(blockNumber)*264
+    }
+
+function distanceTravelledInFeet(blockNumber,blockDestination){
+        if(blockNumber > blockDestination){
+            return (blockNumber-blockDestination)*264
+        }
+        return (blockDestination-blockNumber)*264
+    }
+
+function calculatesFarePrice(blockNumber,blockDestination){
+    if(distanceTravelledInFeet(blockNumber,blockDestination) <= 400){
+        return 0
+    }else if(distanceTravelledInFeet(blockNumber,blockDestination) <= 2000){
+        return (distanceTravelledInFeet(blockNumber,blockDestination) - 400) * 0.02
+    }else if(distanceTravelledInFeet(blockNumber,blockDestination) <= 2500){
+        return 25
+    }else if(distanceTravelledInFeet(blockNumber,blockDestination) > 2500){
+        return 'cannot travel that far'
+    }
+}
+
